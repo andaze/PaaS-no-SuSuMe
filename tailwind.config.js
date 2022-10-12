4b9d917bf853a1bacb8f203293ef36f4fab9f9e5
Nishikawa Daisaku <nishidai@andaze.com>
2022-10-12 17:53:10 +0900

    Update HUGO-base assets

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './content/**/*.{md,html,js}',
+    './themes/**/*.{md,html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+}
