474da6537896f0ccf9a2f765be9b54c6f2f79107
TomoyukiHata <hatayuki-andaze.com>
2022-11-08 10:02:46 +0900

    edit JS file

--- themes/andaze/assets/js/download.js
+++ themes/andaze/assets/js/download.js
@@ -1,6 +1,5 @@
 'use strict'
     {
-        console.log('download!')
         const target = document.querySelector('.multi-download');
         target.addEventListener('click', downloadFiles);
     
@@ -13,28 +12,30 @@
         // 画像ダウンロード
         function downloadFile(fileName){
             // ファイル情報
-            const url = '../pdf/' + fileName;;
-        
+            const url = '../pdf/' + fileName;
+            
             // IE用
             if(window.navigator.msSaveOrOpenBlob){
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.responseType = 'blob';
-            xhr.onload = function(){
-                if(xhr.status !== 200){
-                return false;
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                xhr.responseType = 'blob';
+                xhr.onload = function(){
+                    if(xhr.status !== 200){
+                        return false;
+                    }
+                    window.navigator.msSaveOrOpenBlob(xhr.response, fileName);
                 }
-                window.navigator.msSaveOrOpenBlob(xhr.response, fileName);
-            }
             xhr.send();
-            }else{ // Chromeなど
-            let link = document.createElement('a');
-            link.href= url;
-            link.download = fileName;
-        
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
+
+            } else { // Chromeなど
+
+                let link = document.createElement('a');
+                link.href= url;
+                link.download = fileName;
+            
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
             }
         }
     }
